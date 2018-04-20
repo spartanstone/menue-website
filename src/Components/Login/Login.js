2b474009b689b5b67cc69eddd4b3c208ccd6768c
spartanstone <arin.ser.stepanian@gmail.com>
2018-04-19 22:20:21 -0700

    login page
    login page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,7 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'material-ui';
 import './Login.css';
-import SwipeableViews from 'react-swipeable-views';
 import LoginTab from './LoginTab';
 
-const styles = {
-  headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
-  },
-  slide: {
-    padding: 10,
-  },
-};
 
 class Login extends Component {
 
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -2,27 +2,96 @@ import React, { Component } from 'react';
 import {TextField,Button  } from 'material-ui';
 import './Login.css';
 import PropTypes from 'prop-types';
+import {AccountCircle,Email,Lock,} from '@material-ui/icons';
+import Input, { InputAdornment } from 'material-ui/Input';
+import { withStyles } from 'material-ui/styles';
+import Icon from 'material-ui/Icon';
+
 
 const styles = theme => ({
-  button: {
-    margin: theme.spacing.unit,
-  },
-  margin: 12,
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+        width: 400,
+    },
+    button: {
+        margin: theme.spacing.unit,
+        width: 300,
+        height: 60,
+
+
+
+
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+    iconSmall: {
+        fontSize: 15,
+    },
 });
 class LoginForm extends Component {
 
   render() {
+      const { classes } = this.props;
     return (
     	<div>
-        <Button variant="raised" component="span" >
-          Login
-        </Button>
+          <TextField
+              required
+              id="EmailInput"
+              label="Email"
+              className={classes.textField}
+              helperText="Please Enter Your Email"
+              fullWidth
+              margin="normal"
+              InputProps={{
+                  startAdornment: (
+                      <InputAdornment position="start">
+                        <Email />
+                      </InputAdornment>
+                  ),
+              }}
 
+          />
+          <TextField
+              required
+              id="password-input"
+              label="Password"
+              className={classes.textField}
+              type="password"
+              autoComplete="current-password"
+              margin="normal"
+              InputProps={{
+                  className: classes.TextColor,
+                  startAdornment: (
+                      <InputAdornment position="start">
+                        <Lock />
+                      </InputAdornment>
+                  ),
+              }}
+              helperText="Please Enter Your Password"
+          />
+
+          <div >
+            <Button variant="raised" color="primary" className={classes.button}>
+              Login
+              <Icon className={classes.rightIcon}>send</Icon>
+            </Button>
+          </div>
     	</div>
+
 
     );
   }
 }
+LoginForm.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(LoginForm);
 
 
-export default LoginForm;
